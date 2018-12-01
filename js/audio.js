40ae5967d4c0680d0cf7f59b7e4b9b2e2b0dac16
Eilson He <yhe01@wesleyan.edu>
2018-12-01 17:17:09 -0500

    Nothing

--- js/audio.js
+++ js/audio.js
@@ -1,39 +1,35 @@
-import './loader.js'
+var audioContext = new AudioContext();
+var oscillator = audioContext.createOscillator();
+oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
+oscillator.type = "sine";
 
-// var AudioContext = window.AudioContext || window.webkitAudioContext;
-// var context = new AudioContext();
-
-// var oscillator = new context.createOscillator();
-// var gain = new context.createGain();
-
-const trackArray = [];
-
-function Track(instrument) {
-    this.onPlay = 'false';
-    this.instrument = instrument;
-    trackArray.push(this);
-}
-
-const playButton = document.getElementById("play");
-playButton.onclick = function () {
-    if(trackArray.length == 0) {
-        var track1 = new Track("acoustic_grand_piano");
-        track1.onPlay = 'true';
-    }
-    track.onPlay = "true";
-    playTrack();
-}
-
-function() {
-
-}
-
-
-
-MIDI.loadPlugin(onsuccess);
-// simple example to get started;
-MIDI.loadPlugin({
-    instrument: "acoustic_grand_piano", // or the instrument code 1 (aka the default)
-    instruments: [ "acoustic_grand_piano", "acoustic_guitar_nylon" ], // or multiple instruments
-    onsuccess: function() { }
-});
+// // var AudioContext = window.AudioContext || window.webkitAudioContext;
+// // var context = new AudioContext();
+//
+// // var oscillator = new context.createOscillator();
+// // var gain = new context.createGain();
+// const trackArray = [];
+// const table = [(C0, 44), (D0, 45), (E0, 46), (F0, 47), (G0, 48), (A0, 49), (B0, 50), (C1, 51)]
+//
+//
+// MIDI.loadPlugin(onsuccess);
+// MIDI.loadPlugin({
+//     instrument: "acoustic_grand_piano",
+//     onsuccess: function() { }
+// });
+//
+// function Track(instrument) {
+//     this.onPlay = 'false';
+//     this.instrument = instrument;
+//     trackArray.push(this);
+// }
+//
+// const playButton = document.getElementById("play");
+// playButton.onclick = function () {
+//     if(trackArray.length == 0) {
+//         var track1 = new Track("acoustic_grand_piano");
+//         track1.onPlay = 'true';
+//     }
+//     track.onPlay = "true";
+//     playTrack();
+// }
